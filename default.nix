e2b9b7409a926e384419eb2ec0c31023d09d84f8
Ardyan Satya <ardyan.satya@gmail.com>
2025-04-17 02:28:56 +0700

    fix: correct flake.nix and defualt.nix

--- default.nix
+++ default.nix
@@ -1,12 +1,1 @@
-{ stdenv }:
-
-stdenv.mkDerivation {
-  pname = "xenlism-grub-theme";
-  version = "1.0";
-  src = ./.;
-
-  installPhase = ''
-    mkdir -p $out/grub/themes/xenlism
-    cp -r ./Xenlism-Nixos/. $out/grub/themes/xenlism
-  '';
-}
+(import ./flake.nix).outputs.packages.x86_64-linux.default
--- flake.nix
+++ flake.nix
@@ -2,8 +2,19 @@
   description = "Xenlism GRUB theme for NixOS";
 
   outputs = { self, nixpkgs }: {
-    packages = nixpkgs.lib.genAttrs ["x86_64-linux"] (system: {
-      default = nixpkgs.legacyPackages.${system}.callPackage ./default.nix {};
-    });
+    packages.x86_64-linux.default = 
+      let 
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+      in
+        pkgs.stdenv.mkDerivation {
+          pname = "xenlism-grub-theme";
+          version = "1.0";
+          src = ./Xenlism-Nixos;
+
+          installPhase = ''
+            mkdir -p $out/grub/themes
+            cp -r $src $out/grub/themes/xenlism
+          '';
+        };
   };
 }
